8bd7f958139efbf16ecf639a804f5f3c6d53658f
namke-dev <namke.dev@gmail.com>
2024-01-19 00:20:36 +0700

    inbound test controller

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,15 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/c/[controller]")]
     public class PlatformsController : ControllerBase
     {
+        public PlatformsController()
+        {
 
+        }
+        [HttpGet]
+        public ActionResult TestInboundConnection()
+        {
+            //access url: http://localhost:5266/api/c/platforms to test endpoint
+            Console.WriteLine("--> Inbound POST # Commands Service");
+            return Ok("Inbound test PlatformsController of CommandsService");
+        }
     }
 }
