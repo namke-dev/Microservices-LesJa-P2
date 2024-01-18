fecca8897f8457226b45fce9e629ca32a3cd59fa
namke-dev <namke.dev@gmail.com>
2024-01-19 03:31:28 +0700

    implement SendPlatformToCommand

--- PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -1,16 +1,35 @@
+using System.Text;
+using System.Text.Json;
 using PlatformService.Dtos;
 
 namespace PlatformService.SyncDataServices.Http
 {
     public class HttpCommandDataClient : ICommandDataClient
     {
-        public HttpCommandDataClient(HttpClient httpClient)
-        {
+        private HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
 
-        }
-        public Task SendPlatformToCommand(PlatformReadDto platform)
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        public async Task SendPlatformToCommand(PlatformReadDto platform)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(platform),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync("http://localhost:5266/api/c/platforms", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                System.Console.WriteLine("--> Sync POST to CommandsService succeeded");
+            }
+            else
+            {
+                System.Console.WriteLine("--> Sync POST to CommandsService failded");
+            }
         }
     }
 }
